498292c0e3c5485bc84068b6681cdb9777b120af
Gabi1818 <jaresova.gabinka@gmail.com>
2025-04-24 20:25:21 +0200

    Add project files.

---
+++ Snake/GameGrid.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    enum LastSnakeMoveEnum { Right, Left, Up, Down };
+    internal class GameGrid
+    {
+        Random random = new Random();
+        LastSnakeMoveEnum _lastSnakeMove;
+
+        int _rows;
+        int _columns;
+
+        Queue<(int x, int y)> _snake;
+        bool _snakeAlive;
+
+        int _growthPending;
+        bool _isAppleGolden;
+        int _appleX;
+        int _appleY;
+
+        int _totalApplesEaten;
+
+        public GameGrid(int rows, int columns) 
+        {
+            _rows = rows;
+            _columns = columns;
+
+            _snake = new Queue<(int x, int y)>();
+            _snake.Enqueue(( 0, 0 ));
+
+            _growthPending = 0;
+            _snakeAlive = true;
+
+            SpawnNewApple();
+            _totalApplesEaten = 0;
+            _isAppleGolden = false;
+
+
+            _lastSnakeMove = LastSnakeMoveEnum.Down; //snake starts moving down
+        }
+
+        public void MoveSnakeInTheLastDirection()
+        {
+            if(_lastSnakeMove == LastSnakeMoveEnum.Right)
+            {
+                MoveSnakeRight();
+            }
+            else if (_lastSnakeMove == LastSnakeMoveEnum.Left)
+            {
+                MoveSnakeLeft();
+            }
+            else if (_lastSnakeMove == LastSnakeMoveEnum.Up)
+            {
+                MoveSnakeUp();
+            }
+            else if (_lastSnakeMove == LastSnakeMoveEnum.Down)
+            {
+                MoveSnakeDown();
+            }
+        }
+
+        public bool GetSnakeIsAlike()
+        {
+            return _snakeAlive;
+        }
+
+        public IEnumerable<(int x, int y)> GetSnakeBody()
+        {
+            return _snake;
+        }
+
+        public (int x, int y) GetApplePosition()
+        {
+            return (_appleX, _appleY);
+        }
+
+        public int GetTotalApplesEaten()
+        {
+            return _totalApplesEaten;
+        }
+
+        public bool GetIsAppleGolden()
+        {
+            return _isAppleGolden;
+        }
+
+        public void MoveSnakeLeft()
+        {
+            if (_lastSnakeMove == LastSnakeMoveEnum.Right)
+            {
+                return; //player is trying to move the snake back to its current possition
+            }
+
+            var head = _snake.Last();
+            if (_snake.Last().x == 0) //check for wall
+            {
+                _snakeAlive = false;
+                return;
+            }
+            CheckIfTheSnakeIsEatingApple(head.x - 1, head.y);
+            if (_snake.Contains((head.x - 1, head.y)))
+            {
+                _snakeAlive = false;
+                return;
+            }
+            _snake.Enqueue((_snake.Last().x - 1, _snake.Last().y));
+
+
+            if (_growthPending > 0)
+            {
+                _growthPending--; //do not remove tail, snake is growing
+            }
+            else
+            {
+                _snake.Dequeue(); //remove tail
+            }
+
+            _lastSnakeMove = LastSnakeMoveEnum.Left;
+        }
+
+        public void MoveSnakeRight()
+        {
+            if (_lastSnakeMove == LastSnakeMoveEnum.Left)
+            {
+                return; //player is trying to move the snake back to its current possition
+            }
+
+            var head = _snake.Last();
+            if (_snake.Last().x == _columns - 1) //check for wall
+            {
+                _snakeAlive = false;
+                return;
+            }
+            CheckIfTheSnakeIsEatingApple(head.x + 1, head.y);
+            if (_snake.Contains((head.x + 1, head.y)))
+            {
+                _snakeAlive = false;
+                return;
+            }
+            _snake.Enqueue((_snake.Last().x + 1, _snake.Last().y));
+
+
+            if (_growthPending > 0)
+            {
+                _growthPending--; //do not remove tail, snake is growing
+            }
+            else
+            {
+                _snake.Dequeue(); //remove tail
+            }
+
+            _lastSnakeMove = LastSnakeMoveEnum.Right;
+        }
+
+        public void MoveSnakeUp()
+        {
+            if (_lastSnakeMove == LastSnakeMoveEnum.Down)
+            {
+                return; //player is trying to move the snake back to its current possition
+            }
+
+            var head = _snake.Last();
+            if (_snake.Last().y == 0) //check for wall
+            {
+                _snakeAlive = false;
+                return;
+            }
+            CheckIfTheSnakeIsEatingApple(head.x, head.y - 1);
+            if (_snake.Contains((head.x, head.y - 1)))
+            {
+                _snakeAlive = false;
+                return;
+            }
+            _snake.Enqueue((_snake.Last().x, _snake.Last().y - 1));
+
+
+            if (_growthPending > 0)
+            {
+                _growthPending--; //do not remove tail, snake is growing
+            }
+            else
+            {
+                _snake.Dequeue(); //remove tail
+            }
+
+            _lastSnakeMove = LastSnakeMoveEnum.Up;
+        }
+
+        public void MoveSnakeDown()
+        {
+            if (_lastSnakeMove == LastSnakeMoveEnum.Up)
+            {
+                return; //player is trying to move the snake back to its current possition
+            }
+
+            var head = _snake.Last();
+            if (_snake.Last().y == _rows - 1) //check for wall
+            {
+                _snakeAlive = false;
+                return;
+            }
+            CheckIfTheSnakeIsEatingApple(head.x, head.y + 1);
+            if (_snake.Contains((head.x, head.y + 1)))
+            {
+                _snakeAlive = false;
+                return;
+            }
+            _snake.Enqueue((_snake.Last().x, _snake.Last().y + 1));
+
+
+            if (_growthPending > 0)
+            {
+                _growthPending--; //do not remove tail, snake is growing
+            }
+            else
+            {
+                _snake.Dequeue(); //remove tail
+            }
+
+            _lastSnakeMove = LastSnakeMoveEnum.Down;
+        }
+
+        public void SpawnNewApple()
+        {
+            _appleX = new Random().Next(1, _columns - 1);
+            _appleY = new Random().Next(1, _rows - 1);
+
+
+            int rnd = new Random().Next(6);
+            if (rnd == 3)
+            {
+                _isAppleGolden = true;
+            }
+            else
+            {
+                _isAppleGolden = false;
+            }
+        }   
+
+        public void CheckIfTheSnakeIsEatingApple(int headX, int headY)
+        {
+            if (headX == _appleX && headY == _appleY)
+            {
+                if (_isAppleGolden)
+                {
+                    _totalApplesEaten += 3;
+                    _growthPending += 3;
+                }
+                else
+                {
+                    _totalApplesEaten++;
+                    _growthPending += 1;
+                }
+                SpawnNewApple();
+            }
+        }
+
+    }
+}
---
+++ Snake/MainWindow.xaml.cs
@@ -0,0 +1,243 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Media;
+using System.Reflection.Metadata.Ecma335;
+using System.Windows.Threading;
+using System.Threading;
+
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    enum GamePhaseEnum { Start, Play, End, Stop }
+    public partial class MainWindow : Window
+    {
+        private ImageSource tileSnake = new BitmapImage(new Uri("Assets/TileSnake.png", UriKind.Relative));
+        private ImageSource tileApple = new BitmapImage(new Uri("Assets/TileApple.png", UriKind.Relative));
+        private ImageSource tileEmpty = new BitmapImage(new Uri("Assets/TileEmpty.png", UriKind.Relative));
+        private ImageSource tileGoldenApple = new BitmapImage(new Uri("Assets/TileGoldenApple.png", UriKind.Relative));
+
+        Image[,] tileImages;
+
+
+        private GamePhaseEnum gamePhase;
+        GameGrid gameGrid;
+        int rows = 20;
+        int cols = 20;
+
+        DispatcherTimer timer = new DispatcherTimer();
+        int timerValue;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            gamePhase = GamePhaseEnum.Start;
+
+            //timerValue = 1200;
+            timerValue = 5000;
+
+            timer.Interval = TimeSpan.FromMilliseconds(timerValue);
+            timer.Tick += Timer_Tick;
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            gameGrid.MoveSnakeInTheLastDirection();
+            Play();
+        }
+
+        private void BackgroundMusic_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            // Rewind the audio and play again
+            BackgroundMusic.LoadedBehavior = MediaState.Manual;
+            BackgroundMusic.Position = TimeSpan.Zero;
+            BackgroundMusic.Play();
+        }
+
+        private void NextGamePhase()
+        {
+            if (gamePhase == GamePhaseEnum.Start)
+            {
+                EndGameGrid.Visibility = Visibility.Hidden;
+
+                StartGameGrid.Visibility = Visibility.Hidden;
+                PlayGameGrid.Visibility = Visibility.Visible;
+                gameGrid = new GameGrid(rows, cols);
+                InitCanvas();
+                gamePhase = GamePhaseEnum.Play;
+                NextGamePhase();
+            }
+            else if (gamePhase == GamePhaseEnum.Play)
+            {
+                timer.Start();
+                Play();
+            }
+            else if (gamePhase == GamePhaseEnum.End)
+            {
+                timer.Stop();
+                PlayGameGrid.Visibility = Visibility.Hidden;
+                EndGameGrid.Visibility = Visibility.Visible;
+                
+                if (gameGrid.GetTotalApplesEaten() + 1 >= rows * cols) //all tiles filled, snake cannot grow anymore
+                {
+                    GameOverTxtBlock.Text = "You freak. Go touch some grass.";
+                    StartAgainButton.Content = "No.";
+                }
+                else
+                {
+                    GameOverTxtBlock.Text = "Game Over";
+                    StartAgainButton.Content = "Play Again";
+                }
+            }
+        }
+
+        private void StartGameClick(object sender, RoutedEventArgs e)
+        {
+            gamePhase = GamePhaseEnum.Start;
+            NextGamePhase();
+        }
+
+        private void Play()
+        {
+            bool isSnakeAlive = gameGrid.GetSnakeIsAlike();
+            Draw();
+
+            if (!isSnakeAlive)
+            {
+                gamePhase = GamePhaseEnum.End;
+                NextGamePhase();
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gamePhase == GamePhaseEnum.Play)
+            {
+                switch (e.Key)
+                {
+                    case Key.Left:
+                        gameGrid.MoveSnakeLeft();
+                        Play();
+                        break;
+
+                    case Key.A:
+                        gameGrid.MoveSnakeLeft();
+                        Play();
+                        break;
+
+                    case Key.Right:
+                        gameGrid.MoveSnakeRight();
+                        Play();
+                        break;
+
+                    case Key.D:
+                        gameGrid.MoveSnakeRight();
+                        Play();
+                        break;
+
+                    case Key.Down:
+                        gameGrid.MoveSnakeDown();
+                        Play();
+                        break;
+
+                    case Key.S: 
+                        gameGrid.MoveSnakeDown(); 
+                        Play();
+                        break;
+
+                    case Key.Up:
+                        gameGrid.MoveSnakeUp();
+                        Play();
+                        break;
+
+                    case Key.W:
+                        gameGrid.MoveSnakeUp();
+                        Play();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void InitCanvas()
+        {
+            timerValue = 1200;
+            timer.Interval = TimeSpan.FromMilliseconds(timerValue);
+
+            ApplesTxtBlock.Text = "Apples: " + gameGrid.GetTotalApplesEaten().ToString();
+
+            GameCanvas.Children.Clear();
+            tileImages = new Image[rows, cols];
+
+            double cellWidth = GameCanvas.Width / cols;
+            double cellHeight = GameCanvas.Height / rows;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    var img = new Image
+                    {
+                        Width = cellWidth,
+                        Height = cellHeight,
+                        Source = null 
+                    };
+                    Canvas.SetLeft(img, col * cellWidth);
+                    Canvas.SetTop(img, row * cellHeight);
+                    GameCanvas.Children.Add(img);
+                    tileImages[row, col] = img;
+                }
+            }
+        }
+
+        private void Draw()
+        {
+            double tileWidth = GameCanvas.Width / cols;
+            double tileHeight = GameCanvas.Height / rows;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    tileImages[row, col].Source = tileEmpty; 
+                }
+            }
+
+            foreach (var (x, y) in gameGrid.GetSnakeBody())
+            {
+                Image cellImage = tileImages[y, x];
+                cellImage.Source = tileSnake;
+                cellImage.Width = tileWidth;
+                cellImage.Height = tileHeight;
+            }
+
+            var (appleX, appleY) = gameGrid.GetApplePosition();
+            Image appleImage = tileImages[appleY, appleX];
+            appleImage.Source = gameGrid.GetIsAppleGolden() ? tileGoldenApple : tileApple;
+            appleImage.Width = tileWidth;
+            appleImage.Height = tileHeight;
+
+            ApplesTxtBlock.Text = "Apples: " + gameGrid.GetTotalApplesEaten().ToString();
+
+        }
+
+
+    }
+}
